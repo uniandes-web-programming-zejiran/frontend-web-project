f651fd6654e97cbb9f7a1a0b16d30b702aaa5617
Panis26 <ca.sanchezo1@uniandes.edu.co>
2023-05-10 22:06:39 -0500

    Barra de navegación (no funcional)

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,23 +1,15 @@
-import  Container  from "react-bootstrap/Container";
-import  Row  from "react-bootstrap/Row";
-import  Col  from "react-bootstrap/Col";
 
 
 function Post(){
     return(
-       
-        <Container>
-            <Row className="justify-content-center"> 
-                <Col xs={12} md={8} className="d-flex">
-                    <h1>Post</h1>
-                </Col>
-                <Col xs={6} md={4} className="d-flex">
-                    <h1>Post</h1>
-                </Col>
-            </Row>    
-
-        </Container>
-  
+       <div class="row">
+            <div class="col-4">
+                Algo 
+            </div>
+            <div class="col-8">
+                Algo 2
+            </div>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Post from './components/post/Post';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/navbar/NavBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <Post />
   </React.StrictMode>
 );
 
