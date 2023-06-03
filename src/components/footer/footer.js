e6dd6fe1087ef0df06f6201a12f3d2115d247dd2
Juan Alegría <j.alegria@uniandes.edu.co>
2023-06-02 23:21:11 -0500

    Feat: make footer responsive

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,17 +5,21 @@ import footerImage from './../images/footer-image.png';
 function Footer() {
     return (
         <div className='text-center'>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+            <br />
+            <br />
+            <br />
+            <br />
             <MDBRow>
-                <MDBCol></MDBCol>
-                <MDBCol>
-                    <MDBTypography tag="h3" style={{ fontFamily: "system-ui" }}><FormattedMessage id="Nosotros" /></MDBTypography>
-                    <MDBTypography style={{ fontFamily: "system-ui" }}><FormattedMessage id="FooterSomos" /></MDBTypography>
+                <MDBCol md={2}></MDBCol>
+                <MDBCol md={4}>
+                    <MDBTypography tag="h3" style={{ fontFamily: "system-ui" }}>
+                        <FormattedMessage id="Nosotros" />
+                    </MDBTypography>
+                    <MDBTypography style={{ fontFamily: "system-ui" }}>
+                        <FormattedMessage id="FooterSomos" />
+                    </MDBTypography>
                 </MDBCol>
-                <MDBCol>
+                <MDBCol md={4}>
                     <img
                         src={footerImage}
                         alt='footer'
@@ -23,12 +27,14 @@ function Footer() {
                         style={{ height: '200px', borderColor: '#E25540' }}
                     />
                 </MDBCol>
-                <MDBCol></MDBCol>
+                <MDBCol md={2}></MDBCol>
             </MDBRow>
-            <br></br>
-            <MDBTypography className='my-5' tag="h6" style={{ fontFamily: "system-ui" }}><FormattedMessage id="Contactanos" />: +57 3163782253 - info@EcoWeb.com</MDBTypography>
+            <br />
+            <MDBTypography className='my-5' tag="h6" style={{ fontFamily: "system-ui" }}>
+                <FormattedMessage id="Contactanos" />: +57 3163782253 - info@EcoWeb.com
+            </MDBTypography>
         </div>
-    )
+    );
 }
 
 export default Footer;
